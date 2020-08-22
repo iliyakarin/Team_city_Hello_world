f2f89c6654f85fd5c6bf5fabd68cea871c50e6a8
Iliya Karin <som87k@gmail.com>
2020-08-22 13:47:07 +0300

    Добавьте файлы проекта.

---
+++ Hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
